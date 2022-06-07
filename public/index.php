ba9a7fe846c5cfa0ea3384e7747aade3cd413896
Victor Engström <victor.engstrom@gmail.com>
2022-06-07 14:34:24 +0300

    Products + product

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@
     FROM products
 		";
   $stmt = $pdo->query($sql);
-  $items = $stmt->fetchAll();
+  $products = $stmt->fetchAll();
 ?>
 
 <!doctype html>
@@ -14,25 +14,25 @@
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <!-- <link rel="stylesheet" href="css/styles.css"> -->
+      <link rel="stylesheet" href="css/products.css">
       <title>Shop</title>
     </head>
     <body>
       <div class="container">
         <div class="row">
-          <h2>Shop items</h2>
+          <h2>Shop products</h2>
         </div>
-        <div>
-          <?php foreach($items as $item) { ?>
-            <div class="item">
-              <h4 class="item-title"><?=htmlentities($item['title'])?></h4>
-              <h6><?=htmlentities($item['description'])?></h6>
-              <h6 class="title"><?=htmlentities($item['price'])?></h5>
-              <p class="content"><?=htmlentities($item['stock'])?></p>
-              <!-- <form action="item.php" method="GET">
-                  <input type="hidden" name="itemId" value="<?=htmlentities($item['id']) ?>">
+        <div class="row wrapping">
+          <?php foreach($products as $product) { ?>
+            <div class="product">
+              <h4 class="product-title"><?=htmlentities($product['title'])?></h4>
+              <p><?=htmlentities($product['description'])?></p>
+              <p class="title"><?=htmlentities($product['price'])?></p>
+              <p class="content"><?=htmlentities($product['stock'])?></p>
+              <form action="product.php" method="GET">
+                  <input type="hidden" name="productId" value="<?=htmlentities($product['id']) ?>">
                   <input type="submit" value="Read more">
-              </form> -->
+              </form>
             </div>
           <?php } ?>
         </div>
