ed87725eb6df94f7b95fbf2922a6221f463b8211
norrlandl <lina_holmlund@hotmail.com>
2022-06-08 16:57:09 +0200

    update start

--- public/user/user.php
+++ public/user/user.php
@@ -3,11 +3,67 @@
    $pageTitle = "Användare";
    $pageId    = "user";
 
-   $success = "";
 
-   //  echo "<pre>";
-   //  print_r($_POST);
-   //  echo "</pre>";
+  echo "<pre>";
+  print_r($_POST);
+  echo "</pre>";
+
+   $message = "";
+
+   /**
+   * UPDATE 
+   */
+
+  if (isset($_POST['updateUser'])) {
+   $first_name   = trim($_POST['first_name']);
+   $last_name    = trim($_POST['last_name']);
+   $street       = trim($_POST['street']);
+   $postal_code  = trim($_POST['postal_code']);
+   $city         = trim($_POST['city']);
+   $country      = trim($_POST['country']);
+   $phone        = trim($_POST['phone']);
+   $email        = trim($_POST['email']);
+   $password     = trim($_POST['password']);
+   $confirm      = trim($_POST['confirm']);
+
+   if ($password !== $confirm) {
+      $message .= '
+          <div>
+              Confirmed password incorrect!
+          </div>
+      ';
+  } else {
+
+   $sql = "
+   UPDATE users 
+   SET 
+   first_name  = :first_name, 
+   last_name   = :last_name, 
+   street      = :street, 
+   postal_code = :postal_code, 
+   city        = :city, 
+   country     = :country, 
+   phone       = :phone, 
+   email       = :email, 
+   password    = :password,
+   WHERE id = :id";
+
+   $encryptedPassword = password_hash($password, PASSWORD_BCRYPT, ['cost' => 12]);
+
+   $stmt = $dbconnect->prepare($sql);
+   $stmt->bindParam(":id", $_POST['userID']);
+   $stmt->bindParam(':first_name',   $first_name);
+   $stmt->bindParam(':last_name',    $last_name);
+   $stmt->bindParam(':street',       $street);
+   $stmt->bindParam(':postal_code',  $postal_code);
+   $stmt->bindParam(':city',         $city);
+   $stmt->bindParam(':country',      $country);
+   $stmt->bindParam(':phone',        $phone);
+   $stmt->bindParam(':email',        $email);
+   $stmt->bindParam(':password',     $encryptedPassword );
+   // $stmt->execute();
+ }
+}
 
 
    /**
@@ -15,11 +71,6 @@
    */
 
    if (isset($_POST['deleteUser'])) {
-
-    echo "<pre>";
-    print_r($_POST);
-    echo "</pre>";
-
    $sql = "
    DELETE FROM users 
    WHERE id = :id";
@@ -42,31 +93,55 @@
    $stmt = $dbconnect->query($sql); 
    $user = $stmt->fetch();
 
-   echo "<pre>";
-   print_r($user);
-   echo "</pre>";
+   // echo "<pre>";
+   // print_r($user);
+   // echo "</pre>";
 
 ?>
 
 
-<h1>User.php</h1>
-
-
-
-<p>Du är inloggad</p>
-
-
+<h1>Välkommen <?=htmlentities($user['first_name']) ?></h1>
 
 <?php ?>
 
-<?=$success ?>
+<?=$message ?>
 
-   <h4>Välkommen <?=htmlentities($user['first_name']) ?></h4>
-   <h4>Dina uppgifter</h4>
-   <p><?=htmlentities($user['first_name']) ?></p>
-   <p><?=htmlentities($user['last_name']) ?></p>
-   <p><?=htmlentities($user['city']) ?></p>
-   <p><?=htmlentities($user['id']) ?></p>
+   <h4>Mina uppgifter</h4>
+ 
+<form method="POST" action="">
+   <label for="input">Förnamn:</label> <br>
+   <input type="text" class="text" name="first_name" value="<?=htmlentities($user['first_name']) ?>">   <br>
+  
+  <label for="input">Efternamn:</label> <br>
+  <input type="text" class="text" name="last_name" value="<?=htmlentities($user['last_name']) ?>">  <br>
+  
+  <label for="input">Adress:</label> <br>
+  <input type="text" class="text" name="street" value="<?=htmlentities($user['street']) ?>">  <br>
+
+  <label for="input">Postkod:</label> <br>
+  <input type="text" class="text" name="postal_code" value="<?=htmlentities($user['postal_code']) ?>">  <br>
+
+  <label for="input">Stad:</label> <br>
+  <input type="text" class="text" name="city" value="<?=htmlentities($user['city']) ?>">  <br>
+
+  <label for="input">Land:</label> <br>
+  <input type="text" class="text" name="country" value="<?=htmlentities($user['country']) ?>">  <br>
+  
+  <label for="input">Telefon:</label> <br>
+  <input type="text" class="text" name="phone" value="<?=htmlentities($user['phone']) ?>">  <br>
+
+  <label for="input">Email:</label> <br>
+  <input type="text" class="text" name="email" value="<?=htmlentities($user['email']) ?>">  <br>  
+
+  <label for="input">Lösenord:</label> <br>
+  <input type="password" class="text" name="password">  <br>  
+
+  <label for="input">Upprepa lösenord:</label> <br>
+  <input type="password" class="text" name="confirm">  <br>  <br>
+ 
+   <input type="hidden" name="userID" value="<?=htmlentities($user['id']) ?>">
+   <input type="submit" name="updateUser" value="Uppdatera konto">
+</form>
 
 
    <!-- <h4>Tidigare ordrar</h4>
