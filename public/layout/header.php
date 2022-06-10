e5af305a83f0edfffc9c2ba9c5c1983b7804bb36
Victor Engström <victor.engstrom@gmail.com>
2022-06-10 15:44:07 +0300

    links from root
    kräver att projektet ligger direkt under htdocs i mamp

--- public/layout/header.php
+++ public/layout/header.php
@@ -13,12 +13,12 @@
   <header>
     <nav>
       <div class="row">
-        <a class="link" href="index.php">Products</a>
-        <a class="link" href="admin/index.php">Admin</a>
-        <a class="link" href="admin/users/create-new-user.php">New User</a>
-        <a class="link" href="admin/users/update-user.php">Update User</a>
-        <a class="link" href="admin/products/create-new-product.php">New Product</a>
-        <a class="link" href="admin/products/update-product.php">Update Product</a>
+        <a class="link" href="/kunskapskontroll-2_e-commerce/public/index.php">Products</a>
+        <a class="link" href="/kunskapskontroll-2_e-commerce/public/admin/index.php">Admin</a>
+        <a class="link" href="/kunskapskontroll-2_e-commerce/public/admin/users/create-new-user.php">New User</a>
+        <a class="link" href="/kunskapskontroll-2_e-commerce/public/admin/users/update-user.php">Update User</a>
+        <a class="link" href="/kunskapskontroll-2_e-commerce/public/admin/products/create-new-product.php">New Product</a>
+        <a class="link" href="/kunskapskontroll-2_e-commerce/public/admin/products/update-product.php">Update Product</a>
       </div>
     </nav>
   </header>
