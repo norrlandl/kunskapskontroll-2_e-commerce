fb5d3a535be67df4fe4907cd838b63523c64e08c
Jonathan <rautiojonathan@gmail.com>
2022-06-11 20:32:27 +0200

    dbconnect typo bug fixed

--- public/user/user.php
+++ public/user/user.php
@@ -1,20 +1,20 @@
 <?php
-   require('../../src/dbconnect.php');
-   $pageTitle = "Användare";
-   $pageId    = "user";
+require('../../src/dbconnect.php');
+$pageTitle = "Användare";
+$pageId    = "user";
 
 
-  echo "<pre>";
-  print_r($_POST);
-  echo "</pre>";
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
 
-   $message = "";
+$message = "";
 
-   /**
-   * UPDATE 
-   */
+/**
+ * UPDATE 
+ */
 
-  if (isset($_POST['updateUser'])) {
+if (isset($_POST['updateUser'])) {
    $first_name   = trim($_POST['first_name']);
    $last_name    = trim($_POST['last_name']);
    $street       = trim($_POST['street']);
@@ -32,9 +32,9 @@
               Confirmed password incorrect!
           </div>
       ';
-  } else {
+   } else {
 
-   $sql = "
+      $sql = "
    UPDATE users 
    SET 
    first_name  = :first_name, 
@@ -48,100 +48,100 @@
    password    = :password,
    WHERE id = :id";
 
-   $encryptedPassword = password_hash($password, PASSWORD_BCRYPT, ['cost' => 12]);
+      $encryptedPassword = password_hash($password, PASSWORD_BCRYPT, ['cost' => 12]);
 
-   $stmt = $dbconnect->prepare($sql);
-   $stmt->bindParam(":id", $_POST['userID']);
-   $stmt->bindParam(':first_name',   $first_name);
-   $stmt->bindParam(':last_name',    $last_name);
-   $stmt->bindParam(':street',       $street);
-   $stmt->bindParam(':postal_code',  $postal_code);
-   $stmt->bindParam(':city',         $city);
-   $stmt->bindParam(':country',      $country);
-   $stmt->bindParam(':phone',        $phone);
-   $stmt->bindParam(':email',        $email);
-   $stmt->bindParam(':password',     $encryptedPassword );
-   // $stmt->execute();
- }
+      $stmt = $pdo->prepare($sql);
+      $stmt->bindParam(":id", $_POST['userID']);
+      $stmt->bindParam(':first_name',   $first_name);
+      $stmt->bindParam(':last_name',    $last_name);
+      $stmt->bindParam(':street',       $street);
+      $stmt->bindParam(':postal_code',  $postal_code);
+      $stmt->bindParam(':city',         $city);
+      $stmt->bindParam(':country',      $country);
+      $stmt->bindParam(':phone',        $phone);
+      $stmt->bindParam(':email',        $email);
+      $stmt->bindParam(':password',     $encryptedPassword);
+      // $stmt->execute();
+   }
 }
 
 
-   /**
-   * DELETE 
-   */
+/**
+ * DELETE 
+ */
 
-   if (isset($_POST['deleteUser'])) {
+if (isset($_POST['deleteUser'])) {
    $sql = "
    DELETE FROM users 
    WHERE id = :id";
 
-   $stmt = $dbconnect->prepare($sql);
+   $stmt = $pdo->prepare($sql);
    $stmt->bindParam(":id", $_POST['userID']);
    $stmt->execute();
 
    header("Location: user-login.php");
- }
+}
 
 
-  /**
-   * FETCH 
-   */
-   $sql = "
+/**
+ * FETCH 
+ */
+$sql = "
    SELECT * FROM users
    ";
 
-   $stmt = $dbconnect->query($sql); 
-   $user = $stmt->fetch();
+$stmt = $pdo->query($sql);
+$user = $stmt->fetch();
 
 ?>
 
 <?php include('../layout/header.php'); ?>
-   <h1>Välkommen <?=htmlentities($user['first_name']) ?></h1>
+<h1>Välkommen <?= htmlentities($user['first_name']) ?></h1>
 
-   <?php ?>
+<?php ?>
 
-   <?=$message ?>
+<?= $message ?>
 
-      <h4>Mina uppgifter</h4>
-   
-   <form method="POST" action="">
-      <label for="input">Förnamn:</label> <br>
-      <input type="text" class="text" name="first_name" value="<?=htmlentities($user['first_name']) ?>">   <br>
-   
+<h4>Mina uppgifter</h4>
+
+<form method="POST" action="">
+   <label for="input">Förnamn:</label> <br>
+   <input type="text" class="text" name="first_name" value="<?= htmlentities($user['first_name']) ?>"> <br>
+
    <label for="input">Efternamn:</label> <br>
-   <input type="text" class="text" name="last_name" value="<?=htmlentities($user['last_name']) ?>">  <br>
-   
+   <input type="text" class="text" name="last_name" value="<?= htmlentities($user['last_name']) ?>"> <br>
+
    <label for="input">Adress:</label> <br>
-   <input type="text" class="text" name="street" value="<?=htmlentities($user['street']) ?>">  <br>
+   <input type="text" class="text" name="street" value="<?= htmlentities($user['street']) ?>"> <br>
 
    <label for="input">Postkod:</label> <br>
-   <input type="text" class="text" name="postal_code" value="<?=htmlentities($user['postal_code']) ?>">  <br>
+   <input type="text" class="text" name="postal_code" value="<?= htmlentities($user['postal_code']) ?>"> <br>
 
    <label for="input">Stad:</label> <br>
-   <input type="text" class="text" name="city" value="<?=htmlentities($user['city']) ?>">  <br>
+   <input type="text" class="text" name="city" value="<?= htmlentities($user['city']) ?>"> <br>
 
    <label for="input">Land:</label> <br>
-   <input type="text" class="text" name="country" value="<?=htmlentities($user['country']) ?>">  <br>
-   
+   <input type="text" class="text" name="country" value="<?= htmlentities($user['country']) ?>"> <br>
+
    <label for="input">Telefon:</label> <br>
-   <input type="text" class="text" name="phone" value="<?=htmlentities($user['phone']) ?>">  <br>
+   <input type="text" class="text" name="phone" value="<?= htmlentities($user['phone']) ?>"> <br>
 
    <label for="input">Email:</label> <br>
-   <input type="text" class="text" name="email" value="<?=htmlentities($user['email']) ?>">  <br>  
+   <input type="text" class="text" name="email" value="<?= htmlentities($user['email']) ?>"> <br>
 
    <label for="input">Lösenord:</label> <br>
-   <input type="password" class="text" name="password">  <br>  
+   <input type="password" class="text" name="password"> <br>
 
    <label for="input">Upprepa lösenord:</label> <br>
-   <input type="password" class="text" name="confirm">  <br>  <br>
-   
-      <input type="hidden" name="userID" value="<?=htmlentities($user['id']) ?>">
-      <input type="submit" name="updateUser" value="Uppdatera konto">
-   </form>
+   <input type="password" class="text" name="confirm"> <br> <br>
 
-   <!-- <h4>Tidigare ordrar</h4> -->
-   <form method="POST" action="">
-      <input type="hidden" name="userID" value="<?=htmlentities($user['id']) ?>">
-      <input type="submit" name="deleteUser" value="Radera konto">
-   </form>
+   <input type="hidden" name="userID" value="<?= htmlentities($user['id']) ?>">
+   <input type="submit" name="updateUser" value="Uppdatera konto">
+</form>
+
+<!-- <h4>Tidigare ordrar</h4> -->
+<form method="POST" action="">
+   <input type="hidden" name="userID" value="<?= htmlentities($user['id']) ?>">
+   <input type="submit" name="deleteUser" value="Radera konto">
+</form>
 <?php include('../layout/footer.php'); ?>
