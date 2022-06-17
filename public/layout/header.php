3eb4be85ee91b2ee2015f61dcd48bb9ce807f42b
Jonathan <rautiojonathan@gmail.com>
2022-06-17 19:30:50 +0200

    working w/ header layout & conditional logic

--- public/layout/header.php
+++ public/layout/header.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" type="text/css" href="/kunskapskontroll-2_e-commerce/public/css/styles.css?v=<?php echo time(); ?>">
 
   <style>
@@ -186,14 +186,22 @@
       align-items: center;
       background-color: white;
       color: gray;
-      padding: 0 1rem;
+      padding: 0 2rem;
     }
 
     .search-bar {
+      min-height: 8vh;
       background-color: #EBEBEB;
-      display: flex;
-      justify-content: center;
-      align-items: center;
+      position: relative;
+    }
+
+    .mb-4 {
+      width: 40%;
+      margin: 0 auto;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
     }
 
     #main-nav {
@@ -204,12 +212,31 @@
       display: flex;
     }
 
-    .logo::before {
+    .navbar-brand::before {
       content: "PHOTO";
+      color: #BDBDBD;
     }
 
-    .logo::after {
-      content: "PHOSTER";
+    .navbar-brand::after {
+      content: "POSTERS";
+      color: #BDBDBD;
+    }
+
+    .logo {
+      margin: 0 5px;
+    }
+
+    .cart-icon {
+      position: relative;
+    }
+
+    .fa-shopping-cart {
+      color: white;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      margin-left: 1rem;
     }
   </style>
 </head>
@@ -228,26 +255,34 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse w-100">
+        <!--         <div class="collapse navbar-collapse w-100">
           <ul class="nav navbar-nav w-100">
             <li class="nav-item active">
               <a class="nav-link" href="#">POSTERS</a>
             </li>
           </ul>
-        </div>
-        <a class="navbar-brand order-first order-md-0 mx-0" href="#"><img class="logo" src="/public/img/logo.png" alt="logga"></a>
+        </div> -->
+        <a class="navbar-brand order-first order-md-0 mx-0" href="#"><img class="logo" src="../public/img/logo.png" width="50px" height="50px" alt="logga"></a>
         <div class="collapse navbar-collapse w-100">
           <ul class="nav navbar-nav ml-auto">
             <li class="nav-item">
-              <a class="nav-link" href="#">Mina sidor</a>
+              <a class="nav-link" href="#">MINA SIDOR</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Cart</a>
+            <li class="nav-item cart-icon">
+              <i class="fa fa-shopping-cart"></i>
             </li>
           </ul>
         </div>
       </div>
-
     </nav>
-    <h3>search bar</h3>
+    <div class="search-bar">
+      <form id="search-form" action="#">
+        <div class="input-group mb-4 p-1">
+          <input type="search" placeholder="SÖK MOTIV..." aria-describedby="button-addon3" class="form-control bg-none border-0  rounded-pill">
+          <div class="input-group-append border-0">
+            <button id="button-addon3" type="button" class="btn btn-links"><i class="fa fa-search"></i></button>
+          </div>
+        </div>
+      </form>
+    </div>
   </header>
