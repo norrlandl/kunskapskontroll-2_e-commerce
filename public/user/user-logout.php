efb8f53ddbe753ccee692c20b692acab012896ff
Jonathan <rautiojonathan@gmail.com>
2022-06-18 21:26:13 +0200

    conditional rendering on user header

--- public/user/user-logout.php
+++ public/user/user-logout.php
@@ -1,6 +1,9 @@
 <?php
 require('../../src/config.php');
 
-destroySession(".user/user-login.php?logout");
+$_SESSION = [];
+session_destroy();
+redirect("./user-login.php?logout");
+exit;
 
 include('../layout/footer.php');
