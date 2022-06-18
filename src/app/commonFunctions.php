f2309d68a203155afd0f18b62807ec3233a6ed7a
Jonathan <rautiojonathan@gmail.com>
2022-06-18 10:08:22 +0200

    started with logout user

--- src/app/commonFunctions.php
+++ src/app/commonFunctions.php
@@ -7,15 +7,23 @@ function debug($value)
     echo "</pre>";
 }
 
+/* function destroySession($path)
+{
+    session_destroy();
+    redirect($path);
+    exit;
+}
+
+ */
+function redirect($path)
+{
+    header("Location: {$path}");
+    exit;
+}
+
 /* function checkLoginSession($loginParameter, $path)
 {
     if (!isset($_SESSION["$loginParameter"])) {
         redirect($path);
     }
 */
-
-function redirect($path)
-{
-    header("Location: {$path}");
-    exit;
-}
