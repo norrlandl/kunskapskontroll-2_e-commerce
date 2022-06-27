5b0432cd522973ad8ef5e8b304d2f3966acf8a1b
norrlandl <lina_holmlund@hotmail.com>
2022-06-27 12:56:14 +0200

    confirm page html

--- public/cart/order-confirmation.php
+++ public/cart/order-confirmation.php
@@ -16,24 +16,85 @@ unset($_SESSION['cartItems'])
 ?>
 
 
-</head>
+<?php include('../layout/header.php'); ?>
 
-<body>
-  <div class="container">
-    <?php include('cart.php') ?>
+<div class="container">
+  <div class="row">
+    <div class="info">
+      <p><b>Din order har genomförts!</b></p>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
+    </div>
+  </div>
 
 
-    <h4>Confirm</h4>
+  <div class="container-small">
+    <h4>Order #3</h4>
+    <table class="table table-hover table-checkout">
+
+      <thead>
+        <tr>
+          <th scope="col" width="140"></th>
+          <th scope="col" width="300" colspan="2">Poster</th>
+          <!-- <th scope="col"></th> -->
+          <th scope="col">Antal</th>
+          <th scope="col">Summa</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+
+      <tbody>
 
 
-    <?php foreach ($cartItems as $item) : ?>
 
-      <img src="img/<?= $item['img_url'] ?>">
-      <p><?= $item['title'] ?></p>
-      <p><?= $item['description'] ?></p>
-      <p><?= $item['quantity'] ?></p>
-      <p><?= $item['price'] ?></p>
-      <p>Totalsumman: <?= $totalSum ?></p>
+        <?php foreach ($cartItems as $item) : ?>
 
+          <tr>
+            <td>
+              <!-- <div class="checkout-img">
+                <img src="/kunskapskontroll-2_e-commerce/public/img/<?= $item['img_url'] ?>">
+              </div> -->
+            </td>
+            <td colspan="2">
+              <p><?= $item['title'] ?></p>
+            </td>
+            <!-- <td>
+              <p><?= $item['price'] ?></p>
+            </td> -->
+            <td>
+              <p><?= $item['quantity'] ?></p>
+            </td>
+            <td>
+              <p><?= $item['price'] * $item['quantity'] ?>kr </p>
+            </td>
+            <td>
+            </td>
+          </tr>
+      </tbody>
 
     <?php endforeach; ?>
+
+    <tfoot>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>
+          <!-- <p>Antal: <?= $cartTotalItems  ?></p> -->
+        </td>
+        <td colspan="2">
+          <p>Totalsumma: <?= $totalSum ?>kr</p>
+        </td>
+      </tr>
+    </tfoot>
+    </table>
+
+    <form action="/kunskapskontroll-2_e-commerce/public/index.php">
+      <input type="submit" class="btn btn-primary" value="Till startsidan" />
+    </form>
+
+  </div>
+</div>
+
+<?php include('../layout/footer.php'); ?>
